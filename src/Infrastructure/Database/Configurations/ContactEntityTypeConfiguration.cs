1feca73cc25eb38f4e1808adc9c08b03db9297fe
Vitor Marcos da Rosa Custodio <vitor.custodio@xpi.com.br>
2020-08-20 09:18:54 -0300

    Adicionando configuracoes das entidades para entity

---
+++ src/Infrastructure/Database/Configurations/ContactEntityTypeConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace Infrastructure.Database.Configurations
+{
+    internal class ContactEntityTypeConfiguration : IEntityTypeConfiguration<Contact>
+    {
+        public void Configure(EntityTypeBuilder<Contact> builder)
+        {
+            builder.ToTable("TbContact");
+
+            builder.Property<Guid>("CustomerId")
+                .IsRequired();
+
+            builder.Property(f => f.Type)
+                .UsePropertyAccessMode(PropertyAccessMode.Field)
+                .IsRequired();
+
+            builder.Property(f => f.Value)
+                .UsePropertyAccessMode(PropertyAccessMode.Field)
+                .IsRequired()
+                .HasMaxLength(250);
+
+            builder.HasKey("CustomerId", nameof(Contact.Type), nameof(Contact.Value));
+        }
+    }
+}
---
+++ src/Infrastructure/Database/Configurations/CustomerEntityTypeConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Database.Configurations
+{
+    internal class CustomerEntityTypeConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.ToTable("TbCustomer");
+
+            builder.HasKey(f => f.Id)
+                .HasName("CustomerId");
+
+            builder.Property(f => f.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.HasMany(f => f.Contacts)
+                .WithOne()
+                .HasForeignKey("CustomerId")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            var navigation = builder.Metadata.FindNavigation(nameof(Customer.Contacts));
+
+            navigation.SetPropertyAccessMode(PropertyAccessMode.Field);
+        }
+    }
+}
