f6a2ee904b6307c2c8aa0bb0611d434da265506f
Vitor Marcos da Rosa Custodio <vitor.custodio@xpi.com.br>
2020-08-22 14:22:52 -0300

    Teste de repositorio

--- tests/UnitTests/Infrastructure/Repositories/CustomerRepositoryTests.cs
+++ tests/UnitTests/Infrastructure/Repositories/CustomerRepositoryTests.cs
@@ -1,17 +1,57 @@
-﻿using Infrastructure.Database.Context;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Domain;
+using FluentAssertions;
+using Infrastructure.Database.Context;
+using Infrastructure.Repositories.Customer;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using static UnitTests.Infrastructure.Repositories.Fakers.CustomerFaker;
 
 namespace UnitTests.Infrastructure.Repositories
 {
     public class CustomerRepositoryTests
     {
-        private readonly DataContext context;
+        private readonly DataContext _context;
+
+        private static DbConnection CreateInMemoryDatabase()
+        {
+            var connection = new SqliteConnection("Filename=:memory:");
+            connection.Open();
+            return connection;
+        }
 
         public CustomerRepositoryTests()
         {
-            var 
+            var contextOptions = new DbContextOptionsBuilder<DataContext>().UseSqlite(CreateInMemoryDatabase()).Options;
+
+            _context = new DataContext(contextOptions);
+
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
         }
+
+
+        [Fact]
+        public async Task ShouldCreateCustomerAndAddOnDatabaseSuccessfully()
+        {
+            //Arrange
+            const int numberOfContacts = 3;
+            var customer = CreateCustomerListFakeWithContacts(numberOfContacts: numberOfContacts).First();
+            var repository = new CustomerRepository(_context);
+
+            //Act
+             await repository.CreateCustomer(customer);
+
+            //Assert
+            var result = await repository.GetCustomerById(customer.Id);
+            result.Should()
+                .NotBeNull().And
+                .BeSameAs(customer).And
+                .Match((f => ((Customer)f).Contacts.Count == numberOfContacts));
+        }
+
     }
 }
---
+++ tests/UnitTests/Infrastructure/Repositories/Fakers/CustomerFaker.cs
@@ -0,0 +1,34 @@
+﻿using Bogus;
+using Domain;
+using System.Collections.Generic;
+using static UnitTests.Domain.FakeObjects.ContactFaker;
+
+namespace UnitTests.Infrastructure.Repositories.Fakers
+{
+    internal static class CustomerFaker
+    {
+        internal static IList<Customer> CreateCustomerListFakeWithoutContacts(int numberOf = 1)
+        {
+            var customerFaker = new Faker<Customer>()
+                                    .StrictMode(false)
+                                    .CustomInstantiator(f => {
+
+                                        return new Customer(f.Person.FullName);
+                                    });
+
+            return customerFaker.Generate(numberOf);
+        }
+
+        internal static IList<Customer> CreateCustomerListFakeWithContacts(int numberOf = 1, int numberOfContacts = 1)
+        {
+            var customerFaker = new Faker<Customer>()
+                                    .StrictMode(false)
+                                    .CustomInstantiator(f => {
+                                        var contacts = GetContactsFake(numberOfContacts);
+                                        return new Customer(f.Person.FullName, contacts: contacts);
+                                    });
+
+            return customerFaker.Generate(numberOf);
+        }
+    }
+}
