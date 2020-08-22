ca50d94061abad4a2e109eca3541c490cd505c51
Vitor Marcos da Rosa Custodio <vitor.custodio@xpi.com.br>
2020-08-22 12:41:46 -0300

    Inclusao do Repository de customer

---
+++ src/Infrastructure/Repositories/Customer/CustomerRepository.cs
@@ -0,0 +1,34 @@
+﻿using Domain;
+using Infrastructure.Database.Context;
+using System;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories.Customer
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        public readonly DataContext _context;
+
+        public CustomerRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateCustomer(Domain.Customer customer)
+        {
+            await _context.Customers.AddAsync(customer);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Domain.Customer> GetCustomerById(Guid customerId)
+        {
+            return await _context.Customers.FindAsync(customerId);
+        }
+
+        public async Task UpdateCustomer(Domain.Customer customer)
+        {
+            _context.Customers.Update(customer);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ tests/UnitTests/Infrastructure/Repositories/CustomerRepositoryTests.cs
@@ -0,0 +1,17 @@
+﻿using Infrastructure.Database.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.Infrastructure.Repositories
+{
+    public class CustomerRepositoryTests
+    {
+        private readonly DataContext context;
+
+        public CustomerRepositoryTests()
+        {
+            var 
+        }
+    }
+}
