1109c8f6c4ab9e8eaa61173ee381551760f26b40
Vitor Marcos da Rosa Custodio <vitor.custodio@xpi.com.br>
2020-08-26 08:58:36 -0300

    Ajuste na disposiçao do Infrastructure ; correçao dos testes ; inicio da criacao da Application

---
+++ src/Application/Boundaries/CreateCustomer/ContactInput.cs
@@ -0,0 +1,10 @@
+﻿using Domain;
+
+namespace Application.Boundaries.CreateCustomer
+{
+    public class ContactInput
+    {
+        public ContactType Type { get; private set; }
+        public string Value { get; private set; }
+    }
+}
---
+++ src/Application/Boundaries/CreateCustomer/CreateCustomerCommand.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.CommandHandlerBase.Contracts;
+using Infrastructure.CommandHandlerBase.Messages;
+using System;
+using System.Collections.Generic;
+
+namespace Application.Boundaries.CreateCustomer
+{
+    public class CreateCustomerCommand : Command
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public IList<ContactInput> Contacts { get; set; }
+
+
+        public override IValidationResult Validate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
